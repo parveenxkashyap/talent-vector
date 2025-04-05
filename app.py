0ecb1f862d02b0efde16f768b5699d4a5e55a3d4
parveenxkashyap <inparv@gmail.com>
2025-04-05 10:18:56 +0530

    chore: initial repo scaffold

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="TalentVector",
+    page_icon="📄",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("📄 TalentVector")
